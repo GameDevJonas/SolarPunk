e868ed1623afd5b80545d9d8d733c399e62e6ec6
Torbjørn Svendsen <torbsaudio@gmail.com>
2022-01-29 22:47:26 +0100

    wwise implementation last step

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_PLAYER_FOOTSTEPS = 98439365U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_SWITCHES_FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 833819801U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace PLAYER_SWITCHES_FOOTSTEPS
+
+        namespace PLAYER_SWITCHES_WATER_CHARGE
+        {
+            static const AkUniqueID GROUP = 238911056U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID WATER_CHARGE_01 = 1954192589U;
+                static const AkUniqueID WATER_CHARGE_02 = 1954192590U;
+                static const AkUniqueID WATER_CHARGE_03 = 1954192591U;
+                static const AkUniqueID WATER_CHARGE_04 = 1954192584U;
+                static const AkUniqueID WATER_CHARGE_05 = 1954192585U;
+                static const AkUniqueID WATER_CHARGE_06 = 1954192586U;
+            } // namespace SWITCH
+        } // namespace PLAYER_SWITCHES_WATER_CHARGE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYER_RTPC_FOOTSTEPS = 2216764076U;
+        static const AkUniqueID PLAYER_RTPC_WATER_CHARGE = 1031746375U;
+        static const AkUniqueID UI_RTPC_SOLAR_CHARGE = 675368696U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_MASTER = 520809977U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
